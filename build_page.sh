5211d4893c269e32cd67ca8cfaf7162ded004828
Kokulapalan Wimalanathan <kokul@blunderingbioinformatics.org>
2019-10-27 20:55:50 -0400

    add the changes to buidling the site

--- build_page.sh
+++ build_page.sh
@@ -1,4 +1,3 @@
 #bundle exec gem uninstall jekyll-pubmed
-#bundle install --path vendor/bundle
+bundle install --path vendor/bundle
 bundle exec jekyll build -d site
-cd site && git checkout Jenkinsfile
